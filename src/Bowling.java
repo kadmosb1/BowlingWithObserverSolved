33a3d6922efbae6dc159f189a16016267885453b
Karel J. van der Lelij <33454728+kadmosb1@users.noreply.github.com>
2020-04-30 18:05:01 +0200

    Revert "Bowling als Observer van de Lanes en als Observable voor Status"

--- src/Bowling.java
+++ src/Bowling.java
@@ -1,23 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 
-public class Bowling extends Observable implements Observer {
-
+public class Bowling
+{
 	public final List<Lane> lanes;
 
-	public Bowling() {
-		this.lanes = new ArrayList<> ();
-		this.lanes.add (new Lane (1, this));
-		this.lanes.add (new Lane (2, this));
-		this.lanes.add (new Lane (3, this));
-		this.lanes.add (new Lane (4, this));
-	}
-
-	@Override
-	public void update (Observable o, Object arg) {
-		setChanged ();
-		notifyObservers ();
+	public Bowling()
+	{
+		this.lanes = new ArrayList<>();
+		this.lanes.add(new Lane(1));
+		this.lanes.add(new Lane(2));
+		this.lanes.add(new Lane(3));
+		this.lanes.add(new Lane(4));
 	}
 }
